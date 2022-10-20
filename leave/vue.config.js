b1bca74a2c53315cb00b7946c3cb144c07b1ad6a
“letschong” <1642467543@qq.com>
2022-10-20 16:50:06 +0800

    first commit2

---
+++ leave/vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  transpileDependencies: true,
+  devServer: {
+      proxy: {
+          '/api': {
+              target: 'http://localhost:8082',
+              ws: true,
+              changeOrigin: true
+          }
+      }
+  }
+}
